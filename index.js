fcf6f37ca932cb53ea8779c0feb38fde29ae7c27
Aaron Gao <gaoaaron1@gmail.com>
2023-10-20 10:07:09 -0600

    Added new graphics

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
+/* Course: SENG 513 */
+/* Date: OCT 18, 2023 */
+/* Assignment 2 */
+/* Name: Aaron Gao */
+/* UCID: 30056912 */
 
-canvas.width = 1024;
-canvas.height = 576;
 
-c.fillRect(0, 0, canvas.width, canvas.height);
+//const canvas = document.querySelector('canvas');
+//const c = canvas.getContext('2d');
+
+//canvas.width = 1024;
+//canvas.height = 576;
+
+//c.fillRect(0, 0, canvas.width, canvas.height);
+
+
+
+  //================== FUNCTIONS =======================//
+
