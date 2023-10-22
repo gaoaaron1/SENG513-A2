b53f6b62c2025c2f476573ad7f5179c8d11f02f2
Aaron Gao <gaoaaron1@gmail.com>
2023-10-21 22:25:23 -0600

    Added new pages and assets

---
+++ handlers.js
@@ -0,0 +1,17 @@
+/* Course: SENG 513 */
+/* Date: OCT 18, 2023 */
+/* Assignment 1 */
+/* Name: Aaron Gao */
+/* UCID: 30056912 */
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("startBtn").addEventListener("click", function() {
+        window.location.href = "game.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("tutorialBtn").addEventListener("click", function() {
+        window.location.href = "tutorial.html";
+    });
+});
