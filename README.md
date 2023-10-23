97b549a8c175314929794bc3d954ebece4226461
Aaron Gao <gaoaaron1@gmail.com>
2023-10-22 21:17:44 -0600

    Updated logic, README, screen sizes, etc.

--- README.md
+++ README.md
@@ -1,30 +1,68 @@
 # SENG 513 Assignment 2
- - Aaron Gao
- - 30056912
- - October 12th, 2023
+**Author**: Aaron Gao
+**UCID**: 30056912
+
+## GAME OVERVIEW
+
+**Target Platform**: This game is designed for desktop with two players.
+
+**Game Genre**: fighter, strategy, 2 players, retro game.
+
+### Game Objective
+- The primary goal of players is to defeat one another within the time limit.
+- The player whose health bar points reach 0 first loses.
+
+### Rules of the Game
+- Players need to click the Start button to begin the game.
+- Players have the option to view the instructions page to learn the game mechanics.
+- Player 1 controls the skull warrior with 'a', 's', 'w', and 'd' on the keyboard to move left, right, jump, or attack. 
+- Player 2 controls the pumpkin warrior with 'left-arrow key', 'right-arrow key', 'up-arrow key', and 'down-arrow key' on the keyboard to move left, right, jump, or attack. 
+- If either player attacks, and the opposing player collides with the hit box, then the opposing takes damage. 
+- The first player whose health bar reaches 0 loses. 
+- If time runs out, the player with more health points in their health bar is the winner. 
+-If time runs out, and both players have the same health bar points, then the game will declare the game as "Draw"
+
+## Game Mechanics
+### Player 1
+- 'a': Move left
+- 'd': Move right
+- 's': Attack
+- 'w': Jump
 
 
+### Player 2
+- 'leftArrow': Move left
+- 'rightArrow': Move right
+- 'upArrow': Jump
+- 'downArrow': Attack
 
-# Interactions
-Ensure that there are at least 2-3 significant interactive mechanisms in your implementation. This could be player-to-player interactions, 
-player-to-environment, or any other meaningful gameplay interaction.
+### Instruction button: Direct to instructions page.
+### Start button: Start the game for both players.
+### Back button: Direct back to home page.
 
--I am doing a player-to-player interaction as well as a player-to-environment interaction.
+## UI DESIGN
+### Layout and Structure
+- Logo and Icon
+- Background image
+- Button to direct you to instructions page
+- Button to start the game
+- Game container with a bar container and battlefield container
+- Bar container contains 2 health bars for each player and a timer
+- Battlefield container displays a windows with two players
+- Copyright and author at the bottom
+
+### UI Design
+- Two button navigations, one for instructions page, and one for the game
+- Instructions page provide clear explanation on how to play the game
+- Game page contains 2 health bars for each player and a timer
+- Game page contains a button to direct you back to home page 
+
+### Visual Elements
+- Fighters
+- Map
+- Logo of the game
+- Grass
+- HUD
 
 
-# Depth over Breadth
-It's more important to have a few well-implemented, polished features than many shallow ones. Focus on depth in 
-the mechanics you choose to include.
-
--I will focus on sprite frames.
-
-
-# Clear Milestone
-Set clear milestones for your game. What will be the "win" conditions? Is it a certain number of rounds, points, or 
-achieving a specific goal? Define these clearly so that gameplay has a purpose and endpoint.
-Remember, the aim of this assignment is to demonstrate your understanding of game design, your ability to code 
-specific functionalities, and your creativity. It's not to produce a complete, commercial-grade game. Keep the 
-scope manageable and focus on quality.
-
--Win condition is to defeat your opponent.
 
--- index.js
+++ index.js
@@ -4,38 +4,158 @@
 /* Name: Aaron Gao */
 /* UCID: 30056912 */
   
-const player1 = document.getElementById("player-1");
-const player2 = document.getElementById("player-2");
+  /*===================================== VARIABLES ===================================== */
+  let timer = 30;
+  let timerId;
 
+const player1Elements = document.getElementById("player-1");
+const player2Elements = document.getElementById("player-2");
 
-const startButton = document.getElementById("start-button");
+const gameOver = document.querySelector('#displayText');
+const gravity = 0.7;
 
-
-class Player {
-  constructor(name, health) {
-      this.name = name;
-      this.health = health;
-  }
-
-  attack(enemy) {
-      // Implement attack logic
+class Fighter {
+  constructor(options) {
+    //I plan to add variable declarations to this constructor 
+    //Then I plan to have player 1 and player 2 take on those variables
   }
 }
 
-  
-  function rectangularCollision({ rectangle1, rectangle2 }) {
-  }
-  
-  function determineWinner({ player, enemy, timerId }) {
+const player1 = new Fighter({
+  // properties specific to player 1
+});
 
-  }
+const player2 = new Fighter({
+  // properties specific to player 2
+});
+
+
   
-  let timer = 60
-  let timerId
+function rectangularCollision({ rectangle1, rectangle2 }) {
+  // position rectangle1 corresponding to the attacking collision box for player 1 
+  // position rectangle2 corresponding to the attacking collision box for player 2
+}
+  
+function determineWinner({ player, enemy, timerId }) {
+  // clearTimeout(timerId)
+
+  // if player 1 health and player 2 health are equal:
+    // display tie using query selector
+  // else if player 1 health > player 2 health:
+    // display player 1 wins using query selector
+  // else if player 1 health < player 2 health:
+    // display player 2 wins using query selector
+
+}
+
 
   function decreaseTimer() {
-
+    // if timer > 0: (Check if timer greater than 0)
+      // timerId stores identifier for timer
+      // Set a timer with a 1000ms (1 second) delay
+      // time should decrease by 1 every second.
   }
+
+  function animate() {
+    // updates player 1 position
+    // updates player 2 position
+
+    // set player 1 x velocity to 0
+    // set player 2 x velocity to 0
+
+    /*=========================== Player 1 Movement ========================*/
+    // if ('A' or 'a' key is pressed and player1.lastkey is 'a' or 'A'):
+          // player 1 velocity of x is -5;
+          // player 1 swaps sprite to 'run'
+
+    // if ('D' or 'd' key is pressed and player1.lastkey is 'd' or 'D'):
+          // player 1 velocity of x is -5;
+          // player 1 swaps sprite to 'run'     
+    // else: 
+          // player 1 swaps sprite to idle     
+
+
+    /*=========================== Player 2 Movement ========================*/          
+    // if (left arrow key is pressed and player2.lastkey is 'arrowLeft'):
+          // player 2 velocity of x is -5;
+          // player 2 swaps sprite to 'run'
+
+    // if (right arrow key is pressed and player2.lastkey is 'arrowRight'):
+          // player 2 velocity of x is -5;
+          // player 2 swaps sprite to 'run'     
+    // else: 
+          // player 2 swaps sprite to idle     
+
+    /*=========================== Jump Movement ========================*/          
+    // if player 1 and player 2 velocity is less than 0 
+      // player swaps sprite to jump
+
+    // if player 1 and player 2 velocity is greater than 0
+      // player swaps sprite to fall  
+      
+    /*=========================== Detect Collision ========================*/
+      // Check if there is a collision between the Player 2's attack box and player 1.
+      // Check if enemy is in an attacking state.
+      // Player 2's animation frame is at the second frame.
+      // Player 1 is hit then call takeHit() function
+      // Update player's health bar using a GSAP animation
+      // animate player's health bar (identified by '#playerHealth') by setting its width to player's health percentage
+      
+    
+  }
+
+
+  /*============================= KEYBOARD LISTENERS ======================== */
+  window.addEventListener('keydown', (event) => {
+    // if user 1 is not dead:
+
+      // switch (event.key) {
+          // if press a or A:           
+            // move player 1 left       
+
+          // if press d or D:           
+            // move player 1 right      
+
+          // if press w or W:                                   
+            // player 1 jumps using velocity shift in y direction
+
+          // if press s or S:
+            // call player 1 attack function
+
+        // if user 2 is not dead: 
+
+          // if press left arrow key:
+            // move player 2 left
+
+          // if press right arrow key:
+            // move player 2 right
+
+          // if press up arrow key:
+            // player 2 jumps using velocity shift in y direction
+
+          // if press down arrow key:
+            // call player 2 attack function 
+      // }
+
+    })
+
   
+  window.addEventListener('keyup', (event) => {
+    // switch (event.key) {
+          // if released d or D:           
+            // player 1 stops moving right  
+        
+          // if released a or A:          
+            // player 1 stops moving left  
+    // }
   
+    // enemy keys
+    // switch (event.key) {
+          // if released right arrow:          
+            // player 2 stops moving right  
+        
+          // if released left arrow:            
+            // player 2 stops moving left  
+    // }
+  })
   
