766e8121d5b66bc3dca9e1a26e8411125a3b2262
Aaron Gao <gaoaaron1@gmail.com>
2023-11-02 16:10:08 -0600

    refactor code

--- index.js
+++ index.js
@@ -9,70 +9,15 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 //Global constant
 const gravity = 0.7;
 
-//Create sprite for player and enemy entities
-class Entity {
+const background = new Sprite({
 
-    //Pass in position and velocity for constructor
-    constructor({position, velocity, color = 'red', offset}) {
-        this.position = position;
-        this.velocity = velocity;
-        this.width = 50;
-        this.height = 150;
-        this.lastKey;
-        this.attackBox = {
-            position: {
-                x: this.position.x,
-                y: this.position.y 
-            },
-            offset,
-            width: 100,
-            height: 50,
-        }
-        this.color = color;
-        this.isAttacking;
-        this.health = 100;
-    }
+    position: {
+        x: 0,
+        y: 0
+    },
+    imageSrc: './assets/background.png'
 
-    draw() {
-        c.fillStyle = this.color;
-        c.fillRect(this.position.x, this.position.y, this.width, this.height);
-    
-        // attack box
-        if (this.isAttacking) {
-            c.fillStyle = 'blue'
-            c.fillRect(
-            this.attackBox.position.x, 
-            this.attackBox.position.y, 
-            this.attackBox.width, 
-            this.attackBox.height
-            );
-        }
-    }
-
-    update() {
-        this.draw();
-        this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
-        this.attackBox.position.y = this.position.y;
-
-        //Update positions of our entity
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
-
-        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
-            this.velocity.y = 0;
-        } else {
-            this.velocity.y += gravity;
-        }
-    }
-
-    attack() {
-        this.isAttacking = true;
-
-        setTimeout(() => {
-            this.isAttacking = false;
-        }, 100)
-    }
-}
+})
 
 //Create player
 const player = new Entity({
@@ -124,43 +69,7 @@ const keys = {
     }
 }
 
-function rectangularCollision({rectangle1, rectangle2}) {
-    return (
-        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x && 
-        rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
-        rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
-        rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
-    )
-}
 
-
-function determineWinner({player, enemy, timerId}) {
-    clearTimeout(timerId);
-    document.querySelector('#displayText').style.display = 'flex';
-    if (player.health === enemy.health) {
-        document.querySelector('#displayText').innerHTML = 'Tie';
-    } else if (player.health > enemy.health) {
-        document.querySelector('#displayText').innerHTML = 'Player 1 Wins';           
-    } else if (player.health < enemy.health) {
-        document.querySelector('#displayText').innerHTML = 'Player 2 Wins';           
-    }
-}
-
-let timer = 10;
-let timerId;
-function decreaseTimer() {
-    setTimeout(decreaseTimer, 1000)
-    if (timer > 0) {
-        timerId = setTimeout(decreaseTimer, 1000);
-        timer--;
-        document.querySelector('#timer').innerHTML = timer;
-    }
-
-    if (timer === 0) {
-        determineWinner({player, enemy})
-
-    }
-}
 
 decreaseTimer();
 
@@ -170,6 +79,7 @@ function animate() {
     //Clear canvas
     c.fillStyle = 'black';
     c.fillRect(0, 0, canvas.width, canvas.height);
+    background.update();
 
     //Update our entities
     player.update();
